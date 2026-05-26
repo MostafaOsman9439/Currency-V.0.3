88e682ec94b4a3c19886f69b267659ee0f6aa169
Mustafa <newlife9439@gmail.com>
2026-05-27 00:28:58 +0300

    Clean up: Remove duplicate Currency V.0.3 folder

--- main.js
+++ main.js
@@ -7,40 +7,34 @@ const sarDiv = document.querySelector(".sar");
 const egpDiv = document.querySelector(".egp");
 const swapBtn = document.querySelector(".swap");
 
-// For The Swap Button
 let isEgpFirst = true;
-// To Save Rates
 let cachedRates = null;
 
 async function getRates() {
   try {
     const response = await fetch(
-      "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=c2ec46d03b8941d181f768e5b816e424",
+      "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=c2ec46d03b8941d181f768e5b816e424"
     );
     const currency = await response.json();
     cachedRates = currency.rates;
     calculate();
   } catch (error) {
     console.error("Failed to fetch:", error);
+    egpSpan.innerHTML = "Error";
+    sarSpan.innerHTML = "Error";
   }
 }
 
 function calculate() {
-  // To Stop The Site From Breaking It Means If There Is No Data Do Nothing
   if (!cachedRates) return;
-  // calculate
   egpSpan.innerHTML = Math.round(amountInput.value * cachedRates["EGP"]);
   sarSpan.innerHTML = Math.round(amountInput.value * cachedRates["SAR"]);
 }
 
 base.style.order = "1";
-
 egpDiv.style.order = "2";
-
 sarDiv.style.order = "3";
-
 convertBtn.style.order = "4";
-
 swapBtn.style.order = "5";
 
 swapBtn.addEventListener("click", () => {
@@ -50,5 +44,6 @@ swapBtn.addEventListener("click", () => {
 });
 
 convertBtn.addEventListener("click", calculate);
+amountInput.addEventListener("input", calculate);
 
 getRates();
